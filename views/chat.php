b3d85e7882efa7911e3c85e9d1e2e8a49e19a08f
Mert Ali ÖZ <63559486+MertAlioz@users.noreply.github.com>
2021-08-01 19:03:19 +0300

    Add files via upload

---
+++ views/chat.php
@@ -0,0 +1,98 @@
+<section>
+    <main class="root">
+        <div class="">
+            <div class="header d-fixed">
+                <div class="back-arrow">
+                    <a href="./index.php">
+                    <img src="./Assets/images/back-arrow.svg" alt="" width="30px">
+                     
+                </a>
+                </div>
+                <div class="header-userN">
+                    Mert Ali
+                    <small><sub>Aktif</sub></small>
+                    <div class="Typing">
+                        Yazıyor
+                    </div>
+                </div>
+                <div class="YP12">
+                        <img src="./Assets/images/p_3.jpg" class="img-fluid " alt="" width="100%">
+                    </div>
+            </div>
+            <div class="chat-content">
+                <div class="Y0M1">
+                    <div class="MSGY">
+                        <span>Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </div>
+                <div class="Y1M2">
+                    <div class="MSGO">
+                        <span>Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.</span>
+                    </div>
+                    <i class="bi bi-check-all"></i>
+                </div>
+                <div class="Y0M1">
+                    <div class="MSGY">
+                        <span>Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </div>
+                <div class="Y1M2">
+                    <div class="MSGO">
+                        <span>Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.</span>
+                    </div>
+                    <i class="bi bi-check-all"></i>
+                </div>
+                <div class="Y0M1">
+                    <div class="MSGY">
+                        <span>Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </div>
+                <div class="Y1M2">
+                    <div class="MSGO">
+                        <span>Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.</span>
+                    </div>
+                    <i class="bi bi-check-all"></i>
+                </div>
+                <div class="Y0M1">
+                    <div class="MSGY">
+                        <span>Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.</span>
+                    </div>
+                </div>
+                <div class="Y1M2">
+                    <div class="MSGO">
+                        <span>Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.
+                            Lorem ipsum dolor sit amet.</span>
+                    </div>
+                    <i class="bi bi-check-all"></i>
+                </div>
+            </div>
+            <div class="footer">
+                <div class="asd">
+                <i class="bi bi-columns-gap"></i>
+                </div>
+                <div class="Input-area">
+
+                    <input type="text">
+                </div>
+                <div class="asd">
+                <i class="bi bi-arrow-return-right"></i>
+                </div>
+            </div>
+        </div>
+    </main>
+</section>
---
+++ views/index.php
@@ -0,0 +1,31 @@
+<section>
+    <main class="container-fluid">
+        <div class="row">
+            <div class="d-flex justify-content-center  align-items-center">
+                <div class="contents text-center">
+                    <h4 class="home-page-tİtle">Anonimsin.com</h4>
+                    <small class="home-page-tİtle-desc">Set A nick</small>
+                    <small class="home-page-tİtle-desc">Online  User : 218851</small>
+                    <input type="text" name="user_name" placeholder="Set a Name" id="i_user_name">
+
+                    <div class="button-group">
+                        <button class="btn1">
+                            Location Select
+                        </button>
+                        <button class="btn2">
+                            <a href="./chat.php"> Go Chat</a>
+                        </button>
+                        <button class="btn3">
+                            History
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+        <div class="alerts">
+
+        </div>
+    </main>
+</section>
